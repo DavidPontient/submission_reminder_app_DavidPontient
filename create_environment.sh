49346eecc8a6d38a892c2558bff7d12e384a10f5
David Pontient <d.kayumba1@alustudent.com>
2024-09-29 21:36:05 +0200

    created the script, then run it to create submission_reminder_app with its subdirectories

---
+++ create_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p submission_reminder_app/app &&  touch submission_reminder_app/app/reminder.sh
+mkdir -p submission_reminder_app/app &&  touch submission_reminder_app/app/reminder.sh
+mkdir -p submission_reminder_app/modules &&  touch submission_reminder_app/modules/'function.sh'
+mkdir -p submission_reminder_app/assets &&  touch submission_reminder_app/assets/submissions.txt
+mkdir -p submission_reminder_app/config &&  touch submission_reminder_app/config/config.env
+touch submission_reminder_app/startup.sh
