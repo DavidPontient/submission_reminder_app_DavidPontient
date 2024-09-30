f18f40c2625201b84a24ade34a23e9e8b55f2268
David Pontient <d.kayumba1@alustudent.com>
2024-09-30 18:02:06 +0200

    fixed create_environment.sh

--- create_environment.sh
+++ create_environment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-mkdir -p submission_reminder_app/app &&  touch submission_reminder_app/app/reminder.sh
-mkdir -p submission_reminder_app/app &&  touch submission_reminder_app/app/reminder.sh
-mkdir -p submission_reminder_app/modules &&  touch submission_reminder_app/modules/'function.sh'
-mkdir -p submission_reminder_app/assets &&  touch submission_reminder_app/assets/submissions.txt
-mkdir -p submission_reminder_app/config &&  touch submission_reminder_app/config/config.env
+mkdir -p submission_reminder_app/app && mv ~/Desktop/reminder.sh submission_reminder_app/app/
+mkdir -p submission_reminder_app/modules && mv ~/Desktop/'function.sh' submission_reminder_app/modules/
+mkdir -p submission_reminder_app/assets && mv submissions.txt submission_reminder_app/assets/
+mkdir -p submission_reminder_app/config && mv ~/Desktop/config.env submission_reminder_app/config/
 touch submission_reminder_app/startup.sh
